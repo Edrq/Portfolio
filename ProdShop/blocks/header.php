b8baa0b9aff429a0df88cb069e801884c88da481
Edrq <53882337+Edrq@users.noreply.github.com>
2019-08-11 19:14:15 +0300

    Create header.php

---
+++ ProdShop/blocks/header.php
@@ -0,0 +1,25 @@
+<header>
+	<div id="logo">
+		<a href="main.php" title="Перейти на главную">
+			<span>Н</span>овости
+		</a>
+
+	</div>   
+	<div id="menuHead">
+		<a href="about.php">
+		<div style="margin-right: 5%">О Нас</div> 
+        </a>
+        <a href="feedback.php">
+		<div>Обратная связь</div>
+		</a>
+	</div>
+
+	<div id="regAuth">
+		<a href="reg.php">
+		Регистрация 
+		</a> |
+		<a href="auth.php">
+		 Авторизация
+		 </a>
+	</div>
+</header>
