749d81194207fb227bd7f5a85485a955e2256462
Edrq <53882337+Edrq@users.noreply.github.com>
2019-10-30 16:48:20 +0300

    Update main.php

--- ProdShop/main.php
+++ ProdShop/main.php
@@ -19,12 +19,7 @@ include "blocks/head.php";
 
     <?php   
 
-      $mysqli = new mysqli("localhost", "root", "", "productsbase");
-
-$query = "SELECT * FROM prod ORDER BY 'id' "; //готовим запрос. будем выбирать все из //таблицы news и выбирать по id
-
-$result = $mysqli->query($query); // выполняем запрос query.
-//объект результата сохраняем в $result
+    include "MysqlDB.php";
 
 while ($row = $result->fetch_assoc()) {
 
