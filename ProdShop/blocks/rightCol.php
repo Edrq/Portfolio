4ec979eacc6429e054837d27758827e70e636aa0
Edrq <53882337+Edrq@users.noreply.github.com>
2020-05-26 11:43:23 +0300

    Delete rightCol.php

--- ProdShop/blocks/rightCol.php
+++
@@ -1,8 +0,0 @@
-<div id="rightCol">
-		<div class="banner">
-			<img src="/img/banner_1.jpg" id="B1" alt="Баннер 1" title="Баннер 1">
-		</div>
-		<div class="banner">
-			<img src="/img/banner_2.jpg" id="B2" alt="Баннер 2" title="Баннер 2">
-		</div>
-	</div>
